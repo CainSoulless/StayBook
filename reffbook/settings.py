31df6c6a7e6ad3035f7c43bd9399659f7e204efa
Rodrigo Hormazabal <rod.hormazabal@duocuc.cl>
2024-09-29 19:07:13 -0300

    Fixed static

--- reffbook/settings.py
+++ reffbook/settings.py
@@ -119,7 +119,11 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'core/static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
