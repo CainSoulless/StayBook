00874c5bb5888af5cbb5ee96f198521b9b327fdc
Rodrigo Hormazabal <rod.hormazabal@duocuc.cl>
2025-08-15 15:41:55 -0400

    [EDIT] gitignore proyecto Django

--- reffbook/settings.py
+++ reffbook/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = 'django-insecure-p-uh%+f&+wtdxh(a6s_s1^0_aat&syf@b2jjd15_4*b9m)ae4x
 DEBUG = True
 
 # ALLOWED_HOSTS = ['reffbook-db1c4766b71e.herokuapp.com', 'localhost']
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
 
 
 # Application definition
